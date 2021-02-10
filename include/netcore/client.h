8eaf0d28e66ebff404f21b7ba399c92b4e6fc22e
Gene Koval <genyaremchuk@protonmail.com>
2021-02-10 01:06:53 -0500

    Add support for connecting to remote servers

--- include/netcore/client.h
+++ include/netcore/client.h
@@ -3,5 +3,7 @@
 #include <netcore/socket.h>
 
 namespace netcore {
+    auto connect(std::string_view host, std::string_view port) -> socket;
+
     auto connect(std::string_view path) -> socket;
 }
--- src/libnetcore/client.cpp
+++ src/libnetcore/client.cpp
@@ -2,11 +2,37 @@
 
 #include <cstring>
 #include <ext/except.h>
+#include <netdb.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <sys/un.h>
 #include <timber/timber>
 
 namespace netcore {
+    auto connect(std::string_view host, std::string_view port) -> socket {
+        auto hints = addrinfo();
+        addrinfo* result = nullptr;
+
+        std::memset(&hints, 0, sizeof(hints));
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+
+        getaddrinfo(host.data(), port.data(), &hints, &result);
+
+        auto sock = socket(result->ai_family, result->ai_socktype);
+
+        if (::connect(sock.fd(), result->ai_addr, result->ai_addrlen) == -1) {
+            throw ext::system_error(
+                "failed to connect to: " +
+                std::string(host) + ":" + std::string(port)
+            );
+        }
+
+        DEBUG() << sock << " connected to: " << host << ':' << port;
+
+        return sock;
+    }
+
     auto connect(std::string_view path) -> socket {
         auto addr = sockaddr_un();
         std::memset(&addr, 0, sizeof(addr));
