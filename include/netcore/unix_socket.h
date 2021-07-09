1790a16b10f693cca1695fcbed4207524452da1a
Gene Koval <genyaremchuk@protonmail.com>
2021-07-09 16:16:13 -0400

    Move 'chown' functionality to an external library

--- include/netcore/unix_socket.h
+++ include/netcore/unix_socket.h
@@ -1,19 +1,18 @@
 #pragma once
 
+#include <ext/unix.h>
+
 #include <string>
 #include <filesystem>
 #include <optional>
 #include <variant>
 
 namespace netcore {
-    using owner_t = std::variant<std::string, uid_t>;
-    using group_t = std::variant<std::string, gid_t>;
-
     struct unix_socket {
         std::filesystem::path path;
         std::optional<std::filesystem::perms> mode;
-        std::optional<owner_t> owner;
-        std::optional<group_t> group;
+        std::optional<ext::user> owner;
+        std::optional<ext::group> group;
 
         auto apply_permissions() const -> void;
     };
