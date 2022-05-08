315a176fdc4af8d28e6c702c7fca06a6f46dfa1f
Gene Koval <genyaremchuk@protonmail.com>
2022-05-08 19:50:53 -0400

    Remove unnecessary check before 'chown' call

--- src/libnetcore/unix_socket.cpp
+++ src/libnetcore/unix_socket.cpp
@@ -3,6 +3,6 @@
 namespace netcore {
     auto unix_socket::apply_permissions() const -> void {
         if (mode) std::filesystem::permissions(path, mode.value());
-        if (owner || group) ext::chown(path, owner, group);
+        ext::chown(path, owner, group);
     }
 }
