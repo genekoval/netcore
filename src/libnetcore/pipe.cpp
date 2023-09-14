7c68d2b0c757b54dd6a1602483bf2f5c4a5d489e
Gene Koval <genyaremchuk@protonmail.com>
2023-09-13 22:26:37 -0400

    Fix "src/libnetcore/pipe.cpp" formatting

--- src/libnetcore/pipe.cpp
+++ src/libnetcore/pipe.cpp
@@ -8,34 +8,34 @@
 #include <utility>
 
 namespace netcore {
-        pipe::pipe() {
-            auto descriptors = std::array<int, 2>();
+    pipe::pipe() {
+        auto descriptors = std::array<int, 2>();
 
-            if (::pipe2(descriptors.data(), O_CLOEXEC | O_NONBLOCK)) {
-                throw ext::system_error("Failed to create pipe");
-            }
-
-            read_end = descriptors[0];
-            write_end = descriptors[1];
-
-            TIMBER_TRACE(
-                "pipe (read: {}, write: {}) created",
-                static_cast<int>(read_end),
-                static_cast<int>(write_end)
-            );
+        if (::pipe2(descriptors.data(), O_CLOEXEC | O_NONBLOCK)) {
+            throw ext::system_error("Failed to create pipe");
         }
 
-        auto pipe::read() -> fd {
-            auto r = std::move(read_end);
-            const auto w = std::move(write_end);
+        read_end = descriptors[0];
+        write_end = descriptors[1];
 
-            return r;
-        }
+        TIMBER_TRACE(
+            "pipe (read: {}, write: {}) created",
+            static_cast<int>(read_end),
+            static_cast<int>(write_end)
+        );
+    }
 
-        auto pipe::write() -> fd {
-            const auto r = std::move(read_end);
-            auto w = std::move(write_end);
+    auto pipe::read() -> fd {
+        auto r = std::move(read_end);
+        const auto w = std::move(write_end);
 
-            return w;
-        }
+        return r;
+    }
+
+    auto pipe::write() -> fd {
+        const auto r = std::move(read_end);
+        auto w = std::move(write_end);
+
+        return w;
+    }
 }
